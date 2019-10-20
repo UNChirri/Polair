98bf88651686e43e579900e7b7c6ee17e612ad19
Crowspeak <afcajamarcar@unal.edu.co>
2019-10-20 16:22:01 -0500

    Added background, Polair logo and activity design with integration

--- app/src/main/java/com/polairapp/polair/SplashScreenActivity.kt
+++ app/src/main/java/com/polairapp/polair/SplashScreenActivity.kt
@@ -1,7 +1,12 @@
 package com.polairapp.polair
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
 }
