a41350be60c26eb9a41274900f7fc78bc277fb06
Crowspeak <afcajamarcar@unal.edu.co>
2019-10-22 02:10:52 -0500

    Added pager slider indicator

--- app/src/main/java/com/polairapp/polair/SplashScreenActivity.kt
+++ app/src/main/java/com/polairapp/polair/SplashScreenActivity.kt
@@ -1,11 +1,11 @@
 package com.polairapp.polair
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import androidx.viewpager.widget.ViewPager
+import kotlin.properties.Delegates
 
 class SplashScreenActivity : AppCompatActivity() {
 
@@ -13,12 +13,40 @@ class SplashScreenActivity : AppCompatActivity() {
 
     private lateinit var pagerAdapter: SplashViewPagerAdapter
 
+    private lateinit var rightPagerButton: Button
+
+    private lateinit var leftPagerButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        rightPagerButton = findViewById(R.id.pager_two)
+        leftPagerButton = findViewById(R.id.pager_one)
+        leftPagerButton.isPressed = true
         viewPager = findViewById(R.id.splash_viewpager)
         pagerAdapter = SplashViewPagerAdapter(supportFragmentManager, populateDescriptionList())
         viewPager.adapter = pagerAdapter
+
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+            override fun onPageSelected(position: Int) {
+                if (position == 0){
+                    leftPagerButton.isPressed = true
+                    rightPagerButton.isPressed = false
+                } else {
+                    leftPagerButton.isPressed = false
+                    rightPagerButton.isPressed = true
+                }
+            }
+
+        })
+
     }
 
     private fun  populateDescriptionList() : ArrayList<Fragment>{
