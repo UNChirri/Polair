bffe951d6018b0dee5635107b65a428e055058cf
Crowspeak <afcajamarcar@unal.edu.co>
2019-10-24 23:03:25 -0500

    Added PredictActivity

---
+++ app/src/main/java/com/polairapp/polair/PredictActivity.kt
@@ -0,0 +1,12 @@
+package com.polairapp.polair
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PredictActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_predict)
+    }
+}
