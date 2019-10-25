a9d2bea014d3e9d08feb61df62e5b21fa1352d69
jufarangoma <jufarangoma@gmail.com>
2019-10-25 01:10:19 -0500

    Facebook Login implemented

--- app/src/main/java/com/polairapp/polair/presentation/LoginActivity.kt
+++ app/src/main/java/com/polairapp/polair/presentation/LoginActivity.kt
@@ -1,13 +1,69 @@
 package com.polairapp.polair.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.*
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FirebaseAuth
 import com.polairapp.polair.R
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity: AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var callbackManager: CallbackManager
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+        callbackManager = CallbackManager.Factory.create()
+        signOut()
+
+        initFacebookCallback()
+        initButtonsListeners()
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun initFacebookCallback() {
+        facebookLogin.setReadPermissions("email", "public_profile")
+        facebookLogin.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                startActivity(Intent(this@LoginActivity,MapsActivity::class.java))
+            }
+
+            override fun onCancel() {
+
+                print("")
+            }
+
+            override fun onError(error: FacebookException) {
+
+                print(error)
+            }
+
+        })
+
+    }
+    private fun initButtonsListeners() {
+        consLayFacebook.setOnClickListener {
+                        facebookLogin.performClick()
+        }
+    }
+
+
+    fun signOut() {
+        auth.signOut()
+        LoginManager.getInstance().logOut()
     }
 }
