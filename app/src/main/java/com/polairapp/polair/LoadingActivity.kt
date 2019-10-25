242d7f36c82b52d36ceda5d96295db1449c09587
Crowspeak <afcajamarcar@unal.edu.co>
2019-10-24 19:25:41 -0500

    moved .gif file to raw folder, added 5 seconds delay into LoadingActivity

--- app/src/main/java/com/polairapp/polair/LoadingActivity.kt
+++ app/src/main/java/com/polairapp/polair/LoadingActivity.kt
@@ -1,16 +1,44 @@
 package com.polairapp.polair
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 
 class LoadingActivity : AppCompatActivity(){
 
+    private var mDelayHandler: Handler? = null
+    private val GIF_DELAY: Long = 5000 //5 seconds
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+            startNextActivity()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
-        val imageView : ImageView = findViewById(R.id.air_gif);
-        Glide.with(this).load(R.drawable.loading_bar).into(imageView);
+        val imageView : ImageView = findViewById(R.id.air_gif)
+        Glide.with(this).asGif().load(R.raw.loading_bar).into(imageView)
+        mDelayHandler = Handler()
+        mDelayHandler!!.postDelayed(mRunnable, GIF_DELAY)
+    }
+
+    override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+
+
+    private fun startNextActivity(){
+        val intent = Intent(this, MapsActivity::class.java)
+        finish()
+        startActivity(intent)
     }
 }
