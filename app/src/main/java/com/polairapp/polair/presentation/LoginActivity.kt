f02cece69bb224aa72e424c99d5ca076b10d3ad2
jufarangoma <jufarangoma@gmail.com>
2019-10-24 22:58:59 -0500

    Modify login buttons

---
+++ app/src/main/java/com/polairapp/polair/presentation/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.polairapp.polair.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.polairapp.polair.R
+
+class LoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/polairapp/polair/presentation/MapsActivity.kt
+++ app/src/main/java/com/polairapp/polair/presentation/MapsActivity.kt
@@ -1,4 +1,4 @@
-package com.polairapp.polair
+package com.polairapp.polair.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.polairapp.polair.R
 import kotlinx.android.synthetic.main.activity_maps.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
