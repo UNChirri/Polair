b40cdcbc5efd1380454ebd6f99577e714db1c6ed
Crowspeak <afcajamarcar@unal.edu.co>
2019-10-26 09:09:31 -0500

    Added predecir button, reworked styles and sizes

--- app/src/main/java/com/polairapp/polair/PredictActivity.kt
+++ app/src/main/java/com/polairapp/polair/PredictActivity.kt
@@ -1,12 +1,21 @@
 package com.polairapp.polair
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_predict.*
 
 class PredictActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_predict)
+        initButtonsListeners()
+    }
+
+    private fun initButtonsListeners() {
+        btn_back.setOnClickListener{
+            startActivity(Intent(this, MapsActivity::class.java))
+        }
     }
 }
