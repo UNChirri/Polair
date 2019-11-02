ce04a0eadc98934a17a5771fb3113cbab0daefb2
Crowspeak <afcajamarcar@unal.edu.co>
2019-11-02 08:58:33 -0500

    Added time formatter, pass values to prediction results activity - WIP

--- app/src/main/java/com/polairapp/polair/PredictResultsActivity.kt
+++ app/src/main/java/com/polairapp/polair/PredictResultsActivity.kt
@@ -2,19 +2,48 @@ package com.polairapp.polair
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
 class PredictResultsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_predict_results)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        getString(R.string.prediction_params).format(
+            intent.getStringExtra("gas"),
+            intent.getStringExtra("time")
+        )
     }
 
 
-    override fun onMapReady(p0: GoogleMap?) {
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
 
+        val sydney = LatLng(4.63819,-74.08623)
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,15f))
+
+        //Experimental call - HeatMap
+        //addHeatMap()
     }
 
 }
