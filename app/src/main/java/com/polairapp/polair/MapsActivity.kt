b299f29911a73dc65b17c1244dbb29b30188caf3
jufarangoma <jufarangoma@gmail.com>
2019-11-02 00:33:45 -0500

    Trace Path between 2 points implemented

--- app/src/main/java/com/polairapp/polair/MapsActivity.kt
+++ app/src/main/java/com/polairapp/polair/MapsActivity.kt
@@ -1,6 +1,8 @@
 package com.polairapp.polair
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,21 +12,19 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_maps.*
 import android.view.MotionEvent
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
+import android.view.inputmethod.InputMethodManager
+import androidx.lifecycle.Observer
+import com.google.android.gms.maps.model.*
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, SelectPathFragment.BackListener {
 
     private lateinit var mMap: GoogleMap
     private val selectPathFragment = SelectPathFragment()
+    private var startMarket: MarkerOptions?=null
+    private var finishMarker: MarkerOptions?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +34,37 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, SelectPathFragment
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         initButtonsListeners()
+        initObservers()
+    }
+
+    private fun initObservers() {
+        selectPathFragment.startPath.observe(this, Observer {
+            hideSoftKeyboard()
+            startPath(it)
+        })
+        selectPathFragment.finisPath.observe(this, Observer {
+            hideSoftKeyboard()
+            finishPath(it)
+        })
+    }
+
+    private fun finishPath(latLng: LatLng) {
+        finishMarker = MarkerOptions().apply {
+            position(latLng)
+            icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_endpoint))
+        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,16f))
+        mMap.addMarker(finishMarker)
+    }
+
+    fun startPath(latLng: LatLng) {
+        startMarket = MarkerOptions().apply {
+            position(latLng)
+            icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_bike_color))
+        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,18f))
+        mMap.addMarker(startMarket)
+
     }
 
     private fun initButtonsListeners() {
@@ -54,6 +85,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, SelectPathFragment
             showPath()
         }
 
+        imvSOS.setOnClickListener {
+            drawPath()
+        }
+
         imvDropdown.setOnTouchListener(View.OnTouchListener { v, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
@@ -71,9 +106,42 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, SelectPathFragment
         })
     }
 
+    fun drawPath(){
+
+        onBackPressed()
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(19.415077, -99.188688),14f))
+
+        mMap.addPolyline(
+            PolylineOptions().apply {
+                add(startMarket?.position,
+                    LatLng(19.420971, -99.199891),
+                    LatLng(19.420074, -99.197279),
+                    LatLng(19.418565, -99.193715),
+                    LatLng(19.420941, -99.192623),
+                    LatLng(19.421442, -99.190165),
+                    LatLng(19.421112, -99.189558),
+                    LatLng(19.421356, -99.188253),
+                    LatLng(19.421487, -99.184251),
+                    LatLng(19.422185, -99.181633),
+                    LatLng(19.421952, -99.180217),
+                    LatLng(19.421062, -99.179251),
+                    LatLng(19.421502, -99.178586),
+                    LatLng(19.421730, -99.178768),
+                    LatLng(19.422557, -99.177123),
+                    finishMarker?.position)
+                width(8f)
+                color(Color.BLUE)
+                geodesic(true)
+            }
+        )
+
+
+    }
+
     private fun showPath() {
         replaceFragment(selectPathFragment)
-        imvBurguerMenu.visibility = View.VISIBLE
+        imvBurguerMenu.visibility = View.GONE
         imvDropdown.visibility = View.VISIBLE
         imvTitle.visibility = View.GONE
 
@@ -92,7 +160,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, SelectPathFragment
 
     override fun onBackPressed() {
         super.onBackPressed()
+        hidePath()
+    }
+
+    private fun hidePath(){
         imvDropdown.visibility = View.GONE
+        imvBurguerMenu.visibility = View.VISIBLE
+        imvTitle.visibility = View.VISIBLE
     }
 
     /**
@@ -107,8 +181,20 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, SelectPathFragment
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        val sydney = LatLng(4.63819,-74.08623)
+        val ciudadMexico = LatLng(19.423977, -99.185688)
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,15f))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ciudadMexico,13f))
+    }
+
+    fun hideSoftKeyboard() {
+        val inputMethodManager = getSystemService(
+            Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(
+            currentFocus?.windowToken, 0)
+    }
+
+    companion object{
+        const val START_PATH = 1
+        const val FINISH_PATH = 2
     }
 }
--- app/src/main/java/com/polairapp/polair/SelectPathFragment.kt
+++ app/src/main/java/com/polairapp/polair/SelectPathFragment.kt
@@ -6,11 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.maps.model.LatLng
+import com.polairapp.polair.MapsActivity.Companion.FINISH_PATH
+import com.polairapp.polair.MapsActivity.Companion.START_PATH
 import kotlinx.android.synthetic.main.fragment_select_path.*
 
 class SelectPathFragment: Fragment() {
 
     lateinit var mContext: BackListener
+    var startPath = MutableLiveData<LatLng>()
+    var finisPath = MutableLiveData<LatLng>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,6 +45,38 @@ class SelectPathFragment: Fragment() {
 
         imvBack.setOnClickListener {
             mContext.onBackFragmentClick()
+        }
+
+        edtStartPath.setOnFocusChangeListener{v,bool ->
+            linLayStartPathHistory.visibility = if(bool) View.VISIBLE
+            else View.GONE
+        }
+
+        edtFinishPath.setOnFocusChangeListener{v,bool ->
+            linLayFinishPathHistory.visibility = if(bool) View.VISIBLE
+            else View.GONE
+        }
+        txvNavalClub.setOnClickListener {
+            linLayStartPathHistory.visibility = View.GONE
+            startPath.value = LatLng(19.421086, -99.199854)
+            edtStartPath.setText(txvNavalClub.text)
+        }
+
+        txvAngelaTheatre.setOnClickListener {
+            linLayStartPathHistory.visibility = View.GONE
+            startPath.value = LatLng(19.429074, -99.194224)
+            edtStartPath.setText(txvAngelaTheatre.text)
+        }
+
+        txvCentroCulturalDigital.setOnClickListener {
+            linLayFinishPathHistory.visibility = View.GONE
+            finisPath.value = LatLng(19.422784, -99.175932)
+            edtFinishPath.setText(txvCentroCulturalDigital.text)
+        }
+        txvPresstoParquePolanco.setOnClickListener {
+            linLayFinishPathHistory.visibility = View.GONE
+            finisPath.value = LatLng(19.440345, -99.184166)
+            edtFinishPath.setText(txvPresstoParquePolanco.text)
         }
     }
 
