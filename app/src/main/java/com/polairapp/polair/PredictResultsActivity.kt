a20642a9eef5aeeed8dc93870580726c8d3c7632
Crowspeak <afcajamarcar@unal.edu.co>
2019-11-02 02:23:02 -0500

    Added PredictResultsActivity and some look and feel was done

---
+++ app/src/main/java/com/polairapp/polair/PredictResultsActivity.kt
@@ -0,0 +1,20 @@
+package com.polairapp.polair
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+
+class PredictResultsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+    override fun onMapReady(p0: GoogleMap?) {
+
+    }
+
+}
